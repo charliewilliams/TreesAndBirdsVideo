00a9ed5d5ed2a6466e2f0e100b0866a32cc06094
Charlie Williams <c@charliewilliams.org>
2017-06-29 09:40:24 +0200

    Fix tree drawing & frame rate

--- Display/Trees/TreeStack.java
+++ Display/Trees/TreeStack.java
@@ -7,34 +7,39 @@ import processing.core.*;
 public class TreeStack {
 
 	Flower.Type flowerType;
-	
+
 	private ArrayList<Tree> trees = new ArrayList<Tree>();
 	private PGraphics pg;
 	private PApplet parent;
 	private PVector pos;
-	
+	boolean needsRedraw = false;
+
 	TreeStack(int numChildren, PApplet parent, Note n, int baseIndex, PVector pos) {
-		
+
 		this.parent = parent;
 		this.pos = pos;
-		pg = parent.createGraphics(parent.width, parent.height);
+		pg = parent.createGraphics(parent.width, parent.height, PConstants.P2D);
 		pg.colorMode(PConstants.HSB, 360, 100, 100, 100);
-		
+
 		for (int i = 0; i < numChildren; i++) {
-			
+
 			// Tree(PApplet parent, Note n, int idx, int z, float noiseOffset)
 			trees.add(new Tree(parent, n, baseIndex + i, i, Util.random(1000, 2000)));
 		}
 	}
-	
+
 	void grow(boolean leaves, boolean flowers) {
-		
+
+		needsRedraw = true;
+
 		for (Tree t: trees) {
 			t.grow(false, false);
 		}
 	}
-	
+
 	void addFlower() {
+
+		needsRedraw = true;
 
 		for (Tree t: trees) {
 			if (t.addFlower(flowerType)) {
@@ -45,25 +50,31 @@ public class TreeStack {
 
 	void draw() {
 
-		pg.beginDraw();
-		
-		pg.background(0, 0, 100);
-		
-		pg.pushMatrix();
-		pg.translate(pos.x, pos.y);
-		
-		// Background circle
-		pg.fill(0, 0, 90);
-		pg.ellipse(0, 0, 530, 530); 
-		
-		for (Tree t: trees) {
-			t.draw(pg);
+		if (needsRedraw) {
+
+			pg.beginDraw();
+
+			pg.background(0, 0, 0, 0);
+
+			pg.pushMatrix();
+			pg.translate(pos.x, pos.y);
+
+			// Background circle
+			pg.fill(0, 100, 90, 10);
+			pg.ellipse(0, 0, 200, 200); 
+
+			for (Tree t: trees) {
+				t.draw(pg);
+			}
+
+			pg.popMatrix();
+
+			pg.endDraw();
+
+			needsRedraw = false;
 		}
-		
-		pg.popMatrix();
-		
-		pg.endDraw();
-		
+
+		parent.blendMode(PConstants.BLEND);
 		parent.image(pg, 0, 0);
 	}
 }
