d40b121b17ae8eb8741a29495fd788668be2f76a
Charlie Williams <c@harl.ie>
2019-02-13 13:44:47 +0000

    Colour conversion utilities that I don't think we're using

--- Util/Util.java
+++ Util/Util.java
@@ -36,4 +36,42 @@ public class Util {
 
 		return Math.exp(start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1)));
 	}
+	
+	public static int colorFrom360(float hue, float sat, float bri, float alp) {
+		
+		// Convenience function to convert from 360° hue + 100% sat/bri/alpha
+		// to a 0-255 packed 32-bit int like Processing wants: 
+		int alpha = ((int)(alp * 2.55)) << 24;
+		int brightness = ((int) (bri * 2.55) << 16);
+		int saturation = (int) (sat * 2.55) << 8;
+		int hueAngle = ((int) (hue * 0.7083333333));
+		
+		return alpha | brightness | saturation | hueAngle;
+	}
+	
+	public static int colorFrom255(float hue, float sat, float bri, float alp) {
+		
+		// Convenience function to convert from basic 0-255 hue/sat/bri/alpha
+		// to a packed 32-bit int like Processing wants: 
+		int alpha = (int)alp << 24;
+		int brightness = (int)bri << 16;
+		int saturation = (int)sat << 8;
+		int hueAngle = (int)hue;
+		
+		return alpha | brightness | saturation | hueAngle;
+	}
+	
+	public static int bgraFrom255(float blu, float gre, float red, float alp) {
+		
+		// Convenience function to convert from basic 0-255 hue/sat/bri/alpha
+		// to a packed 32-bit int like Processing wants: 
+		int blue = (int)blu << 24;
+		int green = (int)gre << 16;
+		int redd = (int)red << 8;
+		int alpha = (int)alp;
+		
+		return blue | green | redd | alpha;
+	}
+	
+	
 }
